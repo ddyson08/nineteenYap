f11f588f05dec62cead36d0451c80cf71c5ab907
ddyson08 <fmdasani@gmail.com>
2025-07-15 01:49:58 -0700

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("v1").then(cache => {
-      return cache.addAll(["/", "/index.html", "/styles.css", "/app.js"]);
+      return cache.addAll(["/", "index.html", "styles.css", "app.js"]);
     })
   );
 });
